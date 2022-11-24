36d776626fc635df113a233aa66fcb8d29407371
Alex <alexsa-8@mail.ru>
2022-11-24 21:36:06 +0300

    спасибо за помощь.

--- src/main/java/com/skypro/cartonlinestore/controller/ProductController.java
+++ src/main/java/com/skypro/cartonlinestore/controller/ProductController.java
@@ -1,34 +1,27 @@
 package com.skypro.cartonlinestore.controller;
 
-import com.skypro.cartonlinestore.model.Product;
-import com.skypro.cartonlinestore.record.ProductRequest;
 import com.skypro.cartonlinestore.service.ProductService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collection;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
+@RequestMapping("/store/order")
 public class ProductController {
+
     private final ProductService productService;
 
     public ProductController(ProductService productService) {
-
         this.productService = productService;
     }
 
-    @GetMapping("/store/order/add")
-    public Collection<Product> getAllProducts() {
-
-        return this.productService.getAllProducts();
+    @GetMapping("/add")
+    public String add(@RequestParam("id") List<Integer> ids) {
+        this.productService.addToCart(ids);
+        return "Приняты идентификаторы - " + ids;
     }
 
-    @PostMapping("/store/order/get")
-    public Product addingProduct(@RequestBody ProductRequest productRequest)
-            throws IllegalAccessException {
-        return this.productService.addProduct(productRequest);
+    @GetMapping("/get")
+    public List<Integer> get() {
+        return this.productService.getCartContent();
     }
 }
